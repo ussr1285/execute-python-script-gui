117a6108f53922e112e40efab45654102693eb8d
Minjae Joe <ussr1285@ajou.ac.kr>
2023-08-09 09:23:09 +0900

    import another script

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 import tkinter
+import test
 
 window=tkinter.Tk()
 
-window.title("You Dahee")
+window.title("Make cordova app")
 window.geometry("1024x768+100+100")
-window.resizable(False, False)
+window.resizable(True, True)
 
 # os체크 함수
 def check():
-    os_type_label.config(text= "os type = " + os_type_radio.get() + "\n")
+    os_type_label.config(text= "os type = " + os_type_radio.get() + "")
     global os_type
     os_type = os_type_radio.get()
 
@@ -20,13 +21,17 @@ def showResult():
     appId=appIdText.get()
     appVersion=appVersionText.get()
     onesignalPushId=onesignalPushIdText.get()
+
+    androidInfoArr = []
     if(os_type == "android"):
         appVersionCode=appVersionCodeText.get()
         androidKeystorePath=androidKeystorePathText.get()
         androidKeystorePassword=androidKeystorePasswordText.get()
         androidAlias=androidAliasText.get()
         androidAliasPassword=androidAliasPasswordText.get()
-    result.config(text=os_type+appName+appSite+siteDomain+appId+appVersion+onesignalPushId+appVersionCode+androidKeystorePath+androidKeystorePassword+androidAlias+androidAliasPassword)
+        androidInfoArr = [appVersionCode, androidKeystorePath, androidKeystorePassword, androidAlias, androidAliasPassword]
+    test.funca(os_type, appName, appSite, siteDomain, appId, appVersion, onesignalPushId, androidInfoArr) # 여기서 인자 담아서 다른 소스코드 함수 실행.
+    result.config(text=os_type+appName+appSite+siteDomain+appId+appVersion+onesignalPushId+str(androidInfoArr))
 
 
 os_type_radio=tkinter.StringVar()
@@ -41,82 +46,80 @@ os_type_label=tkinter.Label(window, text="None", height=5)
 os_type_label.pack()
 
 # appName 입력창
-appNameLabel=tkinter.Label(window, text="\nappName")
+appNameLabel=tkinter.Label(window, text="app Name")
 appNameLabel.pack()
 appNameText=tkinter.Entry(window)
 appNameText.bind("<Return>")
 appNameText.pack()
 
 # appSite 입력창
-appSiteLabel=tkinter.Label(window, text="\nappSite")
+appSiteLabel=tkinter.Label(window, text="appSite")
 appSiteLabel.pack()
 appSiteText=tkinter.Entry(window)
 appSiteText.bind("<Return>")
 appSiteText.pack()
 
 # siteDomain 입력창
-siteDomainLabel=tkinter.Label(window, text="\nsiteDomain")
+siteDomainLabel=tkinter.Label(window, text="siteDomain")
 siteDomainLabel.pack()
 siteDomainText=tkinter.Entry(window)
 siteDomainText.bind("<Return>")
 siteDomainText.pack()
 
 # appId 입력창
-appIdLabel=tkinter.Label(window, text="\nappId")
+appIdLabel=tkinter.Label(window, text="appId")
 appIdLabel.pack()
 appIdText=tkinter.Entry(window)
 appIdText.bind("<Return>")
 appIdText.pack()
 
 # appVersion 입력창
-appVersionLabel=tkinter.Label(window, text="\nappVersion")
+appVersionLabel=tkinter.Label(window, text="appVersion")
 appVersionLabel.pack()
 appVersionText=tkinter.Entry(window)
 appVersionText.bind("<Return>")
 appVersionText.pack()
 
 # onesignalPushId 입력창
-onesignalPushIdLabel=tkinter.Label(window, text="\nonesignalPushId")
+onesignalPushIdLabel=tkinter.Label(window, text="onesignalPushId")
 onesignalPushIdLabel.pack()
 onesignalPushIdText=tkinter.Entry(window)
 onesignalPushIdText.bind("<Return>")
 onesignalPushIdText.pack()
 
-
-androidOnlyLabel=tkinter.Label(window, text="\n\n여기부터는 안드로이드 빌드할 때만 입력해주시기 바랍니다.")
+androidOnlyLabel=tkinter.Label(window, text="\n\n여기부터는 안드로이드 빌드할 때만 입력해주시기 바랍니다.\n")
 androidOnlyLabel.pack()
 
-
 # appVersionCode 입력창
-appVersionCodeLabel=tkinter.Label(window, text="\nappVersionCode")
+appVersionCodeLabel=tkinter.Label(window, text="appVersionCode")
 appVersionCodeLabel.pack()
 appVersionCodeText=tkinter.Entry(window)
 appVersionCodeText.bind("<Return>")
 appVersionCodeText.pack()
 
 # androidKeystorePath 입력창
-androidKeystorePathLabel=tkinter.Label(window, text="\nandroidKeystorePath")
+androidKeystorePathLabel=tkinter.Label(window, text="androidKeystorePath")
 androidKeystorePathLabel.pack()
 androidKeystorePathText=tkinter.Entry(window)
 androidKeystorePathText.bind("<Return>")
 androidKeystorePathText.pack()
 
 # androidKeystorePassword 입력창
-androidKeystorePasswordLabel=tkinter.Label(window, text="\nandroidKeystorePassword")
+androidKeystorePasswordLabel=tkinter.Label(window, text="androidKeystorePassword")
 androidKeystorePasswordLabel.pack()
 androidKeystorePasswordText=tkinter.Entry(window)
 androidKeystorePasswordText.bind("<Return>")
 androidKeystorePasswordText.pack()
 
 # androidAlias 입력창
-androidAliasLabel=tkinter.Label(window, text="\nandroidAlias")
+androidAliasLabel=tkinter.Label(window, text="androidAlias")
 androidAliasLabel.pack()
 androidAliasText=tkinter.Entry(window)
 androidAliasText.bind("<Return>")
 androidAliasText.pack()
 
 # androidAliasPassword 입력창
-androidAliasPasswordLabel=tkinter.Label(window, text="\nandroidAliasPassword")
+androidAliasPasswordLabel=tkinter.Label(window, text="androidAliasPassword")
 androidAliasPasswordLabel.pack()
 androidAliasPasswordText=tkinter.Entry(window)
 androidAliasPasswordText.bind("<Return>")
---
+++ test.py
@@ -0,0 +1,8 @@
+print("this is global")
+
+def funca(os_type, appName, appSite, siteDomain, appId, appVersion, onesignalPushId, args):
+    print(args)
+    print("this is func")
+
+if __name__ == "__main__":
+    print("this is main")
